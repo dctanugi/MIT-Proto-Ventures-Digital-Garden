88b73a8e02c88d2088e82161a24b7b11ce00cd93
David Cohen-Tanugi <dctanugi@mit.edu>
2025-04-16 13:46:58 -0400

    Modify 404 page

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -9,6 +9,8 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
       <h1>404</h1>
+      <p>Either this page is private or it doesn't exist.</p>
+      <p>I sometimes link from public notes to one of my private notes (for example, meeting notes), sorry about that!</p>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
